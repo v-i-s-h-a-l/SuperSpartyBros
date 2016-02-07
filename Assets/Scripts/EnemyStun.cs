92bc3920cbb81e44155b82d63dd1e4c4375c974a
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2016-02-08 03:20:31 +0530

    bounce player when stunnig enemy

--- Assets/Scripts/EnemyStun.cs
+++ Assets/Scripts/EnemyStun.cs
@@ -10,6 +10,9 @@ public class EnemyStun : MonoBehaviour {
 		{
 			// tell the enemy to be stunned
 			this.GetComponentInParent<Enemy>().Stunned();
+
+			// bounce sparty off the enemy (aka, jump)
+			other.gameObject.GetComponent<CharacterController2D>().JumpOffEnemy();
 		}
 	}
 }
